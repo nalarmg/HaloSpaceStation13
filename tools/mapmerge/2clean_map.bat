50c78632364a0aec82fedd8f286a7986597da5cc
Gregory Maddra <bloxgate@gmail.com>
2016-12-22 22:48:52 -0600

    Update map merge script for the current map (#195)

--- tools/mapmerge/2clean_map.bat
+++ tools/mapmerge/2clean_map.bat
@@ -2,7 +2,7 @@ SET z_levels=4
 cd 
 
 FOR /L %%i IN (1,1,%z_levels%) DO (
-  java -jar MapPatcher.jar -clean ../../maps/Forward_unto_dawn-%%i.dmm.backup ../../maps/Forward_unto_dawn-%%i.dmm ../../maps/Forward_unto_dawn-%%i.dmm
+  java -jar MapPatcher.jar -clean ../../maps/unsc_frigate-%%i.dmm.backup ../../maps/unsc_frigate-%%i.dmm ../../maps/unsc_frigate-%%i.dmm
 )
 
 pause
